c846bd0a762af32c9c1dec2dbdc6cc68a63ea7e5
Yousef Esmaeili <yousefe660@gmail.com>
2024-01-28 20:24:44 +0330

    Update Content

--- 01 OpenCV/01 Basics/09 opencv_translate.py
+++ 01 OpenCV/01 Basics/09 opencv_translate.py
@@ -6,7 +6,7 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str, default="01 OpenCV/01 Basics/Images/opencv_logo.png",
+ap.add_argument("-i", "--image", type=str, default="./Images/opencv_logo.png",
                 help="path to the input image")
 args = vars(ap.parse_args())
 
